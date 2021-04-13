66b8686348bebc8e6c9e52a34bdb3c3991a08907
Mark Morey <markmorey@usf.edu>
2021-04-12 22:25:46 -0400

    Add project files.

---
+++ Models/TeamModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTeam1.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; } //PK for students
+        public String StudentName { get; set; }
+
+        public float GPA { get; set; }
+
+        public int SchoolId { get; set; } //FK for schools
+
+    }
+
+
+    public class School
+    { //get data from API 
+        public int SchoolId { get; set; } //PK for school
+
+        public string SchoolName { get; set; }
+
+        public List<Student> Students { get; set; } //creating list for 1 to many relationship with students class
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string County { get; set; }
+
+
+    }
+}
